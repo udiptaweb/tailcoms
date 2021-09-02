213214233aeeeb93b17340fecadef7f6e3651364
udiptaweb <udiptagogoi000@gmail.com>
2021-09-02 15:21:51 +0530

    sidebar tabs toggle fixed

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -9,7 +9,7 @@
       <span class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" >Layouts</span>
       </NuxtLink>
       <div class="relative">
-        <button type="button" @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+        <button type="button" @click="toggleComponents" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
           <span>Components</span>
           <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
@@ -26,7 +26,7 @@
         </div>
       </div>
       <div class="relative">
-        <button type="button" @click="open_pages_tab = !open_pages_tab" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+        <button type="button" @click="togglePages" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
           <span>Pages</span>
           <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open_pages_tab, 'rotate-0': !open_pages_tab}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
@@ -48,6 +48,24 @@ export default {
     return{
       open:false,
       open_pages_tab:false,
+    }
+  },
+  methods:{
+    toggleComponents(){
+        if(this.open){
+          this.open = false;
+        }else{
+          this.open = true;
+          this.open_pages_tab = false;
+        }
+    },
+    togglePages(){
+        if(this.open_pages_tab){
+          this.open_pages_tab = false;
+        }else{
+          this.open_pages_tab = true;
+          this.open = false;
+        }
     }
   }
 }
--- docs/_nuxt/static/1630575546/components/breadcrumbs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftailcoms\u002F_nuxt\u002Fstatic\u002F1630575546",layout:"App",error:a,serverRendered:true,routePath:"\u002Fcomponents\u002Fbreadcrumbs",config:{_app:{basePath:"\u002Ftailcoms\u002F",assetsPath:"\u002Ftailcoms\u002F_nuxt\u002F",cdnURL:a}}}}(null));
