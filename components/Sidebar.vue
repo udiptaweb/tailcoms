dd06f39491bb974fbdd3fff54462ae0daa802e23
dhrupad-udipta <udipta.gogoi@dhrupadit.com>
2021-11-17 16:04:46 +0530

    tooltips added

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -36,6 +36,9 @@
       <NuxtLink to="/components/form-inputs">
       <span class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" >Form Inputs</span>
       </NuxtLink>
+      <NuxtLink to="/components/tooltips">
+      <span class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" >Tooltips</span>
+      </NuxtLink>
       <span class="text-gray-400 px-4 font-semibold">Pages</span>
       <NuxtLink to="/pages/login">
       <span class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" >Login</span>
---
+++ pages/components/tooltips.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="w-full flex flex-col justify-start gap-y-2 bg-white dark:bg-gray-800 h-full p-4">
+  <h1 class="text-2xl font-semibold tracking-widest text-gray-700 rounded-lg dark-mode:text-white">Tooltips</h1>
+  <h2 class="text-xl font-semibold tracking-widest text-gray-700 rounded-lg dark-mode:text-white">Examples</h2>
+   <code-and-preview :code="tooltip1"></code-and-preview>
+</div>
+</template>
+
+<script>
+import CodeAndPreview from '../../components/CodeAndPreview.vue';
+export default {
+  components: { CodeAndPreview },
+  layout:'App',
+  computed:{
+    alert8(){
+      return `<div class="bg-red-50 border-l-4 border-red-400 text-red-700 p-4" role="alert">
+          <p class="font-bold">Danger!!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adip!.</p>
+        </div>`;
+    },
+    alert7(){
+      return `<div class="bg-green-50 border-l-4 border-green-400 text-green-700 p-4" role="alert">
+          <p class="font-bold">Success!!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adip!.</p>
+        </div>`;
+    },
+    alert6(){
+      return `<div class="bg-blue-50 border-l-4 border-blue-400 text-blue-700 p-4" role="alert">
+          <p class="font-bold">Info</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adip!.</p>
+        </div>`;
+    },
+    alert5(){
+      return `<div class="bg-yellow-50 border-l-4 border-yellow-400 text-yellow-700 p-4" role="alert">
+          <p class="font-bold">Warning</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adip!.</p>
+        </div>`;
+    },
+    alert4(){
+      return `<div class="bg-red-50 rounded text-red-900 px-4 py-3" role="alert">
+          <div class="flex">
+            <div>
+              <svg class="fill-current h-6 w-6 text-red-500 mr-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M12 2c5.5 0 10 4.5 10 10s-4.5 10-10 10S2 17.5 2 12S6.5 2 12 2m0 2c-1.9 0-3.6.6-4.9 1.7l11.2 11.2c1-1.4 1.7-3.1 1.7-4.9c0-4.4-3.6-8-8-8m4.9 14.3L5.7 7.1C4.6 8.4 4 10.1 4 12c0 4.4 3.6 8 8 8c1.9 0 3.6-.6 4.9-1.7z" /></svg>
+            </div>
+            <div>
+              <p class="text-sm text-red-500">Lorem ipsum dolor sit amet, consectetur adip!.</p>
+            </div>
+          </div>
+        </div>`;
+    },
+    alert3(){
+      return `<div class="bg-yellow-50 rounded text-yellow-900 px-4 py-3" role="alert">
+          <div class="flex">
+            <div>
+              <svg class="fill-current h-6 w-6 text-yellow-500 mr-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M13 14h-2V9h2m0 9h-2v-2h2M1 21h22L12 2L1 21z"/></svg>
+            </div>
+            <div>
+              <p class="text-sm text-yellow-500">Lorem ipsum dolor sit amet, consectetur adip!.</p>
+            </div>
+          </div>
+        </div>`;
+    },
+    alert2(){
+      return `<div class="bg-green-50 rounded text-green-900 px-4 py-3" role="alert">
+          <div class="flex">
+            <div>
+              <svg class="fill-current h-6 w-6 text-green-500 mr-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8l-1.41-1.42z" /></svg>
+            </div>
+            <div>
+              <p class="text-sm text-green-500">Lorem ipsum dolor sit amet, consectetur adip!.</p>
+            </div>
+          </div>
+        </div>`;
+    },
+    tooltip1(){
+      return `<div class='has-tooltip'>
+            <span class='tooltip invisible absolute rounded shadow-lg p-1 bg-black text-white -mt-8 text-xs'>Some Nice Tooltip Text</span>Hover on Me (above)
+        </div>`;
+    }
+  }
+}
+</script>
+<style>
+.has-tooltip:hover .tooltip {
+  @apply visible z-50;
+}
+
+</style>
+
